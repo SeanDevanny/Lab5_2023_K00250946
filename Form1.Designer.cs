762e21e14145bf5c729d63efc9b0ba6dbfc5105a
Garry-Love <garrythelover@gmail.com>
2023-03-27 18:47:21 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Lab5_2023_K00250946
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoadCSVButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.DictionaryTitleLabel = new System.Windows.Forms.Label();
+            this.modifyTextButton = new System.Windows.Forms.Button();
+            this.originalTextBox = new System.Windows.Forms.RichTextBox();
+            this.newTextBox = new System.Windows.Forms.RichTextBox();
+            this.originalFileLabel = new System.Windows.Forms.Label();
+            this.newFileLabel = new System.Windows.Forms.Label();
+            this.invertButton = new System.Windows.Forms.Button();
+            this.modeLabel = new System.Windows.Forms.Label();
+            this.modeDisplayLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LoadCSVButton
+            // 
+            this.LoadCSVButton.Location = new System.Drawing.Point(12, 415);
+            this.LoadCSVButton.Name = "LoadCSVButton";
+            this.LoadCSVButton.Size = new System.Drawing.Size(96, 23);
+            this.LoadCSVButton.TabIndex = 0;
+            this.LoadCSVButton.Text = "Upload CSV";
+            this.LoadCSVButton.UseVisualStyleBackColor = true;
+            this.LoadCSVButton.Click += new System.EventHandler(this.LoadCSVButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 53);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(198, 356);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // DictionaryTitleLabel
+            // 
+            this.DictionaryTitleLabel.AutoSize = true;
+            this.DictionaryTitleLabel.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DictionaryTitleLabel.Location = new System.Drawing.Point(9, 14);
+            this.DictionaryTitleLabel.Name = "DictionaryTitleLabel";
+            this.DictionaryTitleLabel.Size = new System.Drawing.Size(177, 25);
+            this.DictionaryTitleLabel.TabIndex = 2;
+            this.DictionaryTitleLabel.Text = "Dictionary in use :";
+            // 
+            // modifyTextButton
+            // 
+            this.modifyTextButton.Location = new System.Drawing.Point(225, 415);
+            this.modifyTextButton.Name = "modifyTextButton";
+            this.modifyTextButton.Size = new System.Drawing.Size(163, 22);
+            this.modifyTextButton.TabIndex = 3;
+            this.modifyTextButton.Text = "Upload Text File to Modify";
+            this.modifyTextButton.UseVisualStyleBackColor = true;
+            this.modifyTextButton.Click += new System.EventHandler(this.modifyTextButton_Click);
+            // 
+            // originalTextBox
+            // 
+            this.originalTextBox.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.originalTextBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.originalTextBox.Location = new System.Drawing.Point(225, 53);
+            this.originalTextBox.Name = "originalTextBox";
+            this.originalTextBox.ReadOnly = true;
+            this.originalTextBox.Size = new System.Drawing.Size(258, 356);
+            this.originalTextBox.TabIndex = 4;
+            this.originalTextBox.Text = "";
+            // 
+            // newTextBox
+            // 
+            this.newTextBox.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.newTextBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.newTextBox.Location = new System.Drawing.Point(489, 53);
+            this.newTextBox.Name = "newTextBox";
+            this.newTextBox.ReadOnly = true;
+            this.newTextBox.Size = new System.Drawing.Size(258, 356);
+            this.newTextBox.TabIndex = 5;
+            this.newTextBox.Text = "";
+            // 
+            // originalFileLabel
+            // 
+            this.originalFileLabel.AutoSize = true;
+            this.originalFileLabel.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.originalFileLabel.Location = new System.Drawing.Point(221, 18);
+            this.originalFileLabel.Name = "originalFileLabel";
+            this.originalFileLabel.Size = new System.Drawing.Size(99, 21);
+            this.originalFileLabel.TabIndex = 6;
+            this.originalFileLabel.Text = "Original File\r\n";
+            // 
+            // newFileLabel
+            // 
+            this.newFileLabel.AutoSize = true;
+            this.newFileLabel.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newFileLabel.Location = new System.Drawing.Point(485, 18);
+            this.newFileLabel.Name = "newFileLabel";
+            this.newFileLabel.Size = new System.Drawing.Size(73, 21);
+            this.newFileLabel.TabIndex = 7;
+            this.newFileLabel.Text = "New File";
+            // 
+            // invertButton
+            // 
+            this.invertButton.Enabled = false;
+            this.invertButton.Location = new System.Drawing.Point(114, 414);
+            this.invertButton.Name = "invertButton";
+            this.invertButton.Size = new System.Drawing.Size(96, 23);
+            this.invertButton.TabIndex = 8;
+            this.invertButton.Text = "Invert";
+            this.invertButton.UseVisualStyleBackColor = true;
+            this.invertButton.Click += new System.EventHandler(this.invertButton_Click);
+            // 
+            // modeLabel
+            // 
+            this.modeLabel.AutoSize = true;
+            this.modeLabel.Location = new System.Drawing.Point(486, 424);
+            this.modeLabel.Name = "modeLabel";
+            this.modeLabel.Size = new System.Drawing.Size(37, 13);
+            this.modeLabel.TabIndex = 10;
+            this.modeLabel.Text = "Mode:";
+            this.modeLabel.Visible = false;
+            // 
+            // modeDisplayLabel
+            // 
+            this.modeDisplayLabel.AutoSize = true;
+            this.modeDisplayLabel.Location = new System.Drawing.Point(529, 424);
+            this.modeDisplayLabel.Name = "modeDisplayLabel";
+            this.modeDisplayLabel.Size = new System.Drawing.Size(100, 13);
+            this.modeDisplayLabel.TabIndex = 11;
+            this.modeDisplayLabel.Text = "English -> American";
+            this.modeDisplayLabel.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.modeDisplayLabel);
+            this.Controls.Add(this.modeLabel);
+            this.Controls.Add(this.invertButton);
+            this.Controls.Add(this.newFileLabel);
+            this.Controls.Add(this.originalFileLabel);
+            this.Controls.Add(this.newTextBox);
+            this.Controls.Add(this.originalTextBox);
+            this.Controls.Add(this.modifyTextButton);
+            this.Controls.Add(this.DictionaryTitleLabel);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.LoadCSVButton);
+            this.Name = "Form1";
+            this.Text = "English to American Translator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LoadCSVButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label DictionaryTitleLabel;
+        private System.Windows.Forms.Button modifyTextButton;
+        private System.Windows.Forms.RichTextBox originalTextBox;
+        private System.Windows.Forms.RichTextBox newTextBox;
+        private System.Windows.Forms.Label originalFileLabel;
+        private System.Windows.Forms.Label newFileLabel;
+        private System.Windows.Forms.Button invertButton;
+        private System.Windows.Forms.Label modeLabel;
+        private System.Windows.Forms.Label modeDisplayLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5_2023_K00250946
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        // Create a new dictionary to store the csv data
+        private Dictionary<string, string> csvData = new Dictionary<string, string>();
+        // Create a new task so other processes can wait for it to finish
+        private Task loadCsvTask = null;
+        // Mode select
+        private bool mode = true;
+        private string originalText = string.Empty;
+        private string newText = string.Empty;
+        private async void LoadCSVButton_Click(object sender, EventArgs e)
+        {
+            // If the loadCsvTask is already running, wait for it to finish
+            if (loadCsvTask != null && !loadCsvTask.IsCompleted)
+            {
+                await loadCsvTask;
+            }
+            // Create a new instance of OpenFileDialog
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            // Set the filter to only show .csv files
+            openFileDialog.Filter = "CSV files (*.csv)|*.csv";
+
+            // Show the dialog and check if the user clicked OK
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //re-setting csvdata
+                csvData = new Dictionary<string, string>();
+                // Read the contents of the csv file in a separate thread
+                loadCsvTask = Task.Run(() =>
+                {
+                    try
+                    {
+                        using (StreamReader reader = new StreamReader(openFileDialog.FileName))
+                        {
+                            // Loop through each line of the csv file
+                            while (!reader.EndOfStream)
+                            {
+                                // Split the line into columns
+                                string[] columns = reader.ReadLine().Split(',');
+
+                                // Add the data to the dictionary
+                                csvData.Add(columns[0], columns[1]);
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                });
+                await loadCsvTask;
+                //display data in a DataGridView
+                dataGridView1.DataSource = csvData.ToList();
+                dataGridView1.Columns["Key"].HeaderText = "English";
+                dataGridView1.Columns["Value"].HeaderText = "American";
+                modeDisplayLabel.Visible = true;
+                modeLabel.Visible = true;
+                invertButton.Enabled = true;
+            }
+        }
+
+        private async void modifyTextButton_Click(object sender, EventArgs e)
+        {
+            // Create a new instance of OpenFileDialog
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            // Set the filter to only show text files
+            openFileDialog.Filter = "Text files (*.txt)|*.txt";
+
+            // Show the dialog and check if the user clicked OK
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                newTextBox.Clear();
+                originalTextBox.Clear();
+                // Grab filepath so it can't be changed by multithreadded processes or anything else
+                string filePath = openFileDialog.FileName;
+
+                // Get the directory path of the selected file
+                string directoryPath = Path.GetDirectoryName(filePath);
+                // Get the file name of the selected file
+                string fileName = Path.GetFileNameWithoutExtension(filePath);
+                // Get the extension
+                string extention = Path.GetExtension(filePath);
+                // Create a new file path for the modified copy of the file
+                string modifiedFilePath;
+                for (int i = 1; true; ++i)
+                {
+                    modifiedFilePath = Path.Combine(directoryPath, $"{fileName}({i}){extention}");
+                    if (!File.Exists(modifiedFilePath))
+                    {
+                        break;
+                    }
+                }
+                // If the loadCsvTask is already running, wait for it to finish
+                if (loadCsvTask != null && !loadCsvTask.IsCompleted)
+                {
+                    await loadCsvTask;
+                }
+                // disable mode toggle so it doesn't mess with translation
+                invertButton.Enabled = false;
+                //note: any text writing done here will not update until process is finished because an invoke wasn't used.
+                //This runs fast enough that it's not noticable and so an invoke is not needed
+                await Task.Run(() =>
+                {
+                    // Open the text file for reading
+                    using (StreamReader reader = new StreamReader(filePath))
+                    {
+                        // Create a new stream writer for the modified copy of the file
+                        using (StreamWriter writer = new StreamWriter(modifiedFilePath))
+                        {
+                            // Read the file line by line
+                            string line;
+                            originalText = string.Empty;
+                            newText = string.Empty;
+                            while ((line = reader.ReadLine()) != null)
+                            {
+                                //write it out to textbox
+                                originalText += line + "\r";
+                                // Split the line into words
+                                string[] words = line.Split(' ');
+
+                                for (int i = 0; i < words.Length; i++)
+                                {
+                                    if (mode)
+                                    {
+                                        if (csvData.ContainsKey(words[i]))
+                                        {
+                                            words[i] = csvData[words[i]];
+                                        }
+                                    }
+                                    else
+                                    {
+                                        if (csvData.ContainsValue(words[i]))
+                                        {
+                                            words[i] = csvData.FirstOrDefault(x => x.Value == words[i]).Key;
+                                        }
+                                    }
+
+                                }
+
+                                // Join the words back together and write the modified line to the new file and to textbox
+                                string modifiedLine = string.Join(" ", words);
+                                newText += modifiedLine + "\r";
+                                writer.WriteLine(modifiedLine);
+                            }
+                        }
+                    }
+                });
+                //if no data iś grid, don't re-enable button
+                if (dataGridView1.ColumnCount != 0)
+                {
+                    //re-enable invert button
+                    invertButton.Enabled = true;
+                }
+                originalTextBox.Text = originalText;
+                newTextBox.Text = newText;
+            }
+        }
+
+        private void invertButton_Click(object sender, EventArgs e)
+        {
+            //toggle mode button
+            mode = !mode;
+            updateModeUI(mode);
+        }
+
+        private void updateModeUI(bool m)
+        {
+            if (m)
+            {
+                dataGridView1.Columns["Key"].DisplayIndex = 0;
+                dataGridView1.Columns["Value"].DisplayIndex = 1;
+                modeDisplayLabel.Text = "English -> American";
+            }
+            else
+            {
+                dataGridView1.Columns["Key"].DisplayIndex = 1;
+                dataGridView1.Columns["Value"].DisplayIndex = 0;
+                modeDisplayLabel.Text = "American -> English";
+            }
+        }
+    }
+}
